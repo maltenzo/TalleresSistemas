1b83acbc3053cfa0e4bc2662412da2e942f9c3e9
meltenzo <evigneti@gmail.com>
2021-04-09 17:29:19 -0300

    agregado taller ipc

---
+++ tallerICP/pipes/anillo_alu.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char **argv)
+{	
+	int status, pid, n;
+	int buffer[1];
+
+	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
+    
+  	/* COMPLETAR */
+    printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer[0], start);
+    
+    /* COMPLETAR */
+    
+}
